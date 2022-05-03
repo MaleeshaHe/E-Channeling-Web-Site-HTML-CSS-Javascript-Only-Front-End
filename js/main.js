61fe29e6d70cd5cb896e1a3dda7525b0f5ae5b1c
Maleesha <malisha27t@gmail.com>
2022-05-04 00:17:39 +0530

    add animation

--- js/main.js
+++ js/main.js
@@ -20,12 +20,24 @@ const sr = ScrollReveal({
   reset: true,
 });
 
-sr.reveal(".home-text", {});
-sr.reveal(".home-img", { delay: 200 });
-
 sr.reveal(".about-text", {});
 sr.reveal(".heading", {});
 sr.reveal(".about-img", { delay: 200 });
 
-sr.reveal(".footer-widget", {});
-sr.reveal(".copyright-wrapper", {});
+sr.reveal(".title", {});
+
+const left = ScrollReveal({
+  origin: "bottom",
+  distance: "85px",
+  duration: 2000,
+  reset: true,
+});
+
+left.reveal(".team-row", { delay: 200 });
+left.reveal(".member", { delay: 200 });
+
+left.reveal(".footer-widget", {});
+left.reveal(".copyright-wrapper", {});
+
+left.reveal(".home-text", {});
+left.reveal(".home-img", { delay: 200 });
