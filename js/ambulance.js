cd6fbb93b7bfde15a69ae13f7a85b14864208618
Maleesha Herath <malishaherath77@gmail.com>
2022-05-06 13:58:03 +0530

    add

--- js/ambulance.js
+++ js/ambulance.js
@@ -1,42 +1,46 @@
 function formValid() {
   nameValid();
-  numberValid();
 }
+
+var tr = 0;
 
 function nameValid() {
   var letters = /^[A-Za-z ]+$/;
-
   var name = document.getElementById("name").value;
 
+  var numbers = /^[0-9]+$/;
+  var mobileNum = document.getElementById("mobileNum").value;
+
+  var tr = 0;
+
   if (name.match(letters)) {
-    cuteAlert({
-      type: "success",
-      title: "Your Request has been sent",
-      message: "We will contact you soon",
-      buttonText: "Close",
-    });
+    tr = tr + 1;
   } else {
     cuteToast({
       type: "error",
       title: "Name",
       message: "You can enter only Characters",
-      timer: 10000,
+      timer: 5000,
     });
   }
-}
-
-function numberValid() {
-  var numbers = /^[0-9]+$/;
-
-  var mobileNum = document.getElementById("mobileNum").value;
 
   if (mobileNum.match(numbers)) {
+    tr = tr + 1;
   } else {
     cuteToast({
       type: "error",
       title: "Phone Number",
       message: "You can enter only Numbers",
-      timer: 10000,
+      timer: 5000,
+    });
+  }
+
+  if (tr == 2) {
+    cuteAlert({
+      type: "success",
+      title: "Your Request has been sent",
+      message: "We will contact you soon",
+      buttonText: "Okay",
     });
   }
 }
