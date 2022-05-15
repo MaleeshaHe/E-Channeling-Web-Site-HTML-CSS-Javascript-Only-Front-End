c5b63a41f71e292dfadfe76bceb18e344adf0986
Maleesha Herath <malishaherath77@gmail.com>
2022-05-15 17:11:42 +0530

    n

--- js/main.js
+++ js/main.js
@@ -23,6 +23,7 @@ const sr = ScrollReveal({
 sr.reveal(".home-text", {});
 sr.reveal(".home-img", { delay: 200 });
 
+
 sr.reveal(".about-text", {});
 sr.reveal(".heading", {});
 sr.reveal(".about-img", { delay: 200 });
@@ -43,3 +44,9 @@ left.reveal(".member", { delay: 200 });
 
 left.reveal(".footer-widget", {});
 left.reveal(".copyright-wrapper", {});
+
+sr.reveal(".home3-text", {});
+sr.reveal(".home3-img", { delay: 200 });
+
+sr.reveal(".home4-text", {});
+sr.reveal(".home4-img", { delay: 200 });
