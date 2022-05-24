615fe0bc6c59d3b57ef356e16603baf786b6b53c
Maleesha Herath <malishaherath77@gmail.com>
2022-05-24 22:52:32 +0530

    update

--- js/form_script.js
+++ js/form_script.js
@@ -3,9 +3,9 @@ $(window).on("load", function () {
   $(".loader").hide();
 });
 
-function checkEmpty(){
+function ch(){
 
-	var fname = document.getElementById("fname").value;
+  var fname = document.getElementById("fname").value;
   var lname = document.getElementById("lname").value;
   var fnum = document.getElementById("fnum").value;
   var femail = document.getElementById("femail").value;
@@ -13,38 +13,70 @@ function checkEmpty(){
   var doctor = document.getElementById("doctor").value;
   var hospital = document.getElementById("hospital").value;
 
-
-	if(fname == null || fname == ""){
-		alert("Fill the First Name Field!");
+  if(fname == null || fname == ""){
+    cuteToast({
+      type: "error",
+      title: "Name",
+      message: "Fill the First Name Field!",
+      timer: 5000,
+    });
 	}
   else if(lname == null || lname == ""){
-		alert("Fill the Last Name Field!");
+    cuteToast({
+      type: "error",
+      title: "Name",
+      message: "Fill the Last Name Field!",
+      timer: 5000,
+    });
 	}
   else if(fnum == null || fnum == ""){
-		alert("Fill the Mobile Number Field!");
+    cuteToast({
+      type: "error",
+      title: "Name",
+      message: "Fill the Mobile Number Field!",
+      timer: 5000,
+    });
 	}
   else if(femail == null || femail == ""){
-		alert("Fill the Email Field!");
+    cuteToast({
+      type: "error",
+      title: "Name",
+      message: "Fill the Email Field!",
+      timer: 5000,
+    });
 	}
   else if(address == null || address == ""){
-		alert("Fill the Address Field!");
+    cuteToast({
+      type: "error",
+      title: "Name",
+      message: "Fill the Address Field!",
+      timer: 5000,
+    });
 	}
   else if(doctor == null || doctor == ""){
-		alert("Fill the preferred doctor Field!");
+    cuteToast({
+      type: "error",
+      title: "Name",
+      message: "Fill the preferred doctor Field!",
+      timer: 5000,
+    });
 	}
   else if(hospital == null || hospital == ""){
-		alert("Fill the preferred hospital Field!");
+    cuteToast({
+      type: "error",
+      title: "Name",
+      message: "Fill the preferred hospital Field!",
+      timer: 5000,
+    });
 	}
-
 	else{
     cuteAlert({
       type: "success",
-      title: "Your Request has been sent",
-      message: "Thank You !!",
+      title: "Your Appointment has been sent",
+      message: "We will contact you soon",
       buttonText: "Okay",
     });
 	}
-
 }
 
 
@@ -56,7 +88,12 @@ function checkLetterf(){
     return true;
   }
   else{
-    confirm("Please Enter Letters only!");
+    cuteToast({
+      type: "error",
+      title: "Name",
+      message: "You can enter only Characters",
+      timer: 5000,
+    });
     return false;
   }
 }
@@ -69,7 +106,12 @@ function checkLetterl(){
     return true;
   }
   else{
-    confirm("Please Enter Letters only!");
+    cuteToast({
+      type: "error",
+      title: "Name",
+      message: "You can enter only Characters",
+      timer: 5000,
+    });
     return false;
   }
 }
